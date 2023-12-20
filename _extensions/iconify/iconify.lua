93c7fd0e2950c4a08376daa2e109632acacf7010
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2023-12-21 00:33:37 +0100

    chore bump to iconify 1.0.8

--- _extensions/iconify/iconify.lua
+++ _extensions/iconify/iconify.lua
@@ -25,7 +25,7 @@
 local function ensure_html_deps()
   quarto.doc.add_html_dependency({
     name = 'iconify',
-    version = '1.0.7',
+    version = '1.0.8',
     scripts = {"iconify-icon.min.js"}
   })
 end
