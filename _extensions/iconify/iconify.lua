7215115441399dc1663be8a40673a5cb591222d1
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2024-05-01 13:10:34 +0200

    refactor: one warning

--- _extensions/iconify/iconify.lua
+++ _extensions/iconify/iconify.lua
@@ -84,7 +84,7 @@ return {
 
       if #args > 1 and string.find(pandoc.utils.stringify(args[2]), ":") then
         quarto.log.warning(
-          'Use "set:icon" or "set icon" syntax, not both!' ..
+          'Use "set:icon" or "set icon" syntax, not both! ' ..
           'Using "set:icon" syntax and discarding first argument!'
         )
         icon = pandoc.utils.stringify(args[2])
