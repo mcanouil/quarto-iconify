96ecd2aa7770ca4183d2b64d07640ba0eb83b084
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2025-01-01 23:20:44 +0100

    chore: update copyright year

--- _extensions/iconify/iconify.lua
+++ _extensions/iconify/iconify.lua
@@ -1,7 +1,7 @@
 --[[
 # MIT License
 #
-# Copyright (c) Mickaël Canouil
+# Copyright (c) 2025 Mickaël Canouil
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
