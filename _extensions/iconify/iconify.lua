ceaa3ea50840006290391862cc368e638b2e6b74
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2025-10-23 23:43:37 +0200

    refactor: simplify utils module loading
    Streamline the loading of the utils module by removing the intermediate variable for the path resolution.

--- _extensions/iconify/iconify.lua
+++ _extensions/iconify/iconify.lua
@@ -26,8 +26,7 @@
 local EXTENSION_NAME = "iconify"
 
 --- Load utils module
-local utils_path = quarto.utils.resolve_path("_modules/utils.lua")
-local utils = require(utils_path)
+local utils = require(quarto.utils.resolve_path("_modules/utils.lua"):gsub("%.lua$", ""))
 
 --- Flag to track if deprecation warning has been shown
 --- @type boolean
